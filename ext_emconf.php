0bfae64f7751d94411c17ed9debd329e9a1ff968
dmh <dmytro.hrynevych@gmail.com>
2016-05-13 19:52:14 +0300

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Theme Blue Mountain',
+	'description' => 'Sub-Theme Blue Mountain fot t3kit project',
+	'category' => 'templates',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 't3kit Team',
+	'author_email' => '',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php'    => '5.2.0-0.0.0',
+			'typo3'  => '6.2.0-7.7.99',
+			'themes' => '7.0.0-7.2.99',
+			'dyncss' => '0.7.0-0.7.99',
+			'dyncss_less' => '0.7.0-0.7.99',
+			't3kit_extension_tools' => '0.0.0-0.0.0',
+			'fluid_styled_content' => '0.0.0-0.0.0',
+			'gridelements' => '7.0.0-7.2.99',
+			'theme_t3kit' => '1.0.0-2.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+);
+
+?>
